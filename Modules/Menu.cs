a04895ba97e218af3cbb23b15fa4129605d7e78b
Lordmau5 <mail@lordmau5.com>
2026-09-03 10:55:24 +0200

    Ditch the WIP exclamation mark implementation
    Was never really finished by the games' developers, the blue color + "Hinted" text should be fine

--- Modules/Menu.cs
+++ Modules/Menu.cs
@@ -327,16 +327,6 @@ namespace NWArchipelago.Modules
                         }
                         else
                         {
-                            // TODO: Show the orange exclamation mark icon when there's a hint
-                            MenuButtonMission missionButton = Traverse.Create(b).Field("missionRef").GetValue<MenuButtonMission>();
-                            if (missionButton != null)
-                            {
-                                var mbTraverse = Traverse.Create(missionButton);
-                                mbTraverse.Method("OnEnable").GetValue();
-                                GameObject mbGameObject = (GameObject)mbTraverse.Field("_newContentStatusObject").GetValue();
-                                mbGameObject?.SetActive(true);
-                            }
-
                             b.localizedText.SetKey("NWArchipelago/MISSION_NAME", [
                                 new AxKReplacementPair("{PREFIX}", "Hinted Levels", false),
                                 new AxKReplacementPair("{MN}", ""),
@@ -354,16 +344,6 @@ namespace NWArchipelago.Modules
                         {
                             if (missionHasHints)
                             {
-                                // TODO: Show the orange exclamation mark icon when there's a hint
-                                MenuButtonMission missionButton = Traverse.Create(b).Field("missionRef").GetValue<MenuButtonMission>();
-                                if (missionButton != null)
-                                {
-                                    var mbTraverse = Traverse.Create(missionButton);
-                                    mbTraverse.Method("OnEnable").GetValue();
-                                    GameObject mbGameObject = (GameObject)mbTraverse.Field("_newContentStatusObject").GetValue();
-                                    mbGameObject?.SetActive(true);
-                                }
-
                                 b.localizedText.SetKey("NWArchipelago/MISSION_NAME", [
                                     new AxKReplacementPair("{PREFIX}", "NWArchipelago/MISSION_PREFIX"),
                                     new AxKReplacementPair("{MN}", i),
@@ -553,12 +533,6 @@ namespace NWArchipelago.Modules
 
                 if (Logic.Level(ld).IsHinted() && currentMissionID != HINTED_MISSION_ID)
                 {
-                    // TODO: Show the orange exclamation mark icon when there's a hint
-                    var lbTraverse = Traverse.Create(__instance);
-                    lbTraverse.Method("CreateNewContentStatusIcon").GetValue();
-                    GameObject mbGameObject = (GameObject)lbTraverse.Field("_newContentStatusObject").GetValue();
-                    mbGameObject?.SetActive(true);
-
                     __instance._textLevelName_Localized.SetKey("NWArchipelago/LEVEL_NAME", [
                         new AxKReplacementPair("{OG?}", leveldisplay),
                         new AxKReplacementPair("{KEY}", Campaign.levelKey[ld.levelID], false),
